ccdcef21a251b0638e8acf10df9098de25456993
XiaoYao Li <476552238@qq.com>
2016-05-28 18:04:08 +0800

    修复因Header参数构造引发的请求发送失败bug

--- Lib/Async.class.php
+++ Lib/Async.class.php
@@ -9,22 +9,19 @@ class Async {
     public static function get($url, $params=array(), $cookies=array()) {
         $parts = parse_url($url);
         try {
-            $fp = self::createSock($url, $parts);
+            $fp = self::createSock($parts);
         } catch (\Exception $e) {
             echo $e->getMessage();
             return false;
         }
         $query_string = "";
         if (!empty($params)) {
-            $query_string = '?';
-            foreach( $params AS $name => $value ){
-                $query_string .= urlencode( $name ) . "=" . urlencode( $value ) . '&';
-            }
-            $query_string = substr( $query_string, 0, -1 );
+            $query_string = '?' . http_build_query($params);
         }
         $parts['path'] = isset($parts['path']) ? $parts['path'] : '/';
-        $request  = "GET " . $parts['path'].$query_string ." HTTP/1.1\r\n";
-        $request .= "Host: ".$parts['host']."\r\n";
+        $request = "GET {$parts['path']}{$query_string}  / HTTP/1.1\r\n";
+        $request .= "Host: {$parts['host']}\r\n";
+        $request .= "Cache-Control: max-age=0\r\n";
         // $request .= 'User-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.2.1) Gecko/20021204\r\n';
         // $request .= 'Accept: text/xml,application/xml,application/xhtml+xml,';
         // $request .= 'text/html;q=0.9,text/plain;q=0.8,video/x-mng,image/png,';
@@ -33,12 +30,12 @@ class Async {
         // $request .= "Accept-Encoding: gzip, deflate, compress;q=0.9\r\n";
         // $request .= "Accept-Charset: ISO-8859-1, utf-8;q=0.66, *;q=0.66\r\n";
         // $request .= "Keep-Alive: 300\r\n";
-        $request .= "Connection: keep-alive\r\n";
-        $request .= "Referer: http://tp3.hhailuo.com/\r\n";
-        $request .= "Cache-Control: max-age=0\r\n";
+        // $request .= "Referer: http://tp3.hhailuo.com/\r\n";
         if (!empty($cookies)) {
             $request .= parseCookie($cookies) . "\r\n";
         }
+        $request .= "Connection: Close\r\n\r\n";
+
         fwrite($fp, $request);
         fclose($fp);
     }
@@ -46,7 +43,7 @@ class Async {
     public static function post($url, $params=array(), $cookies=array()) {
         $parts = parse_url($url);
         try {
-            $fp = self::createSock($url, $parts);
+            $fp = self::createSock($parts);
         } catch (\Exception $e) {
             echo $e->getMessage();
             return false;
@@ -57,24 +54,19 @@ class Async {
         $request.= "Content-Type: application/x-www-form-urlencoded\r\n";
         $post_string = "";
         if (!empty($params)) {
-            foreach ($params as $key => &$val) {
-              if (is_array($val)) $val = implode(',', $val);
-                $post_params[] = $key.'='.urlencode($val);
-            }
-            $post_string = implode('&', $post_params);
+            $post_string = http_build_query($params);
         }
         $request.= "Content-Length: ".strlen($post_string)."\r\n";
         $request.= "Connection: Close\r\n";
         if (!empty($cookies)) {
-            $request .= parseCookie($cookies) . "\r\n\r\n";
+            $request .= parseCookie($cookies) . "\r\n";
         }
-        $request.= $post_string;
-
+        $request .= "\r\n" . $post_string;
         fwrite($fp, $request);
         fclose($fp);
     }
 
-    protected static function createSock($url, $parts) {
+    protected static function createSock($parts) {
         $fp = fsockopen($parts['host'], isset($parts['port']) ? $parts['port']:80, $errno, $errstr, 1);
         if (!$fp) {
             throw new \Exception("创建sock失败", $errno);
